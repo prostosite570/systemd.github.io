6d3716535d4b486cd6e2c3da74027cb856f8b199
prostosite570 <61553765+prostosite570@users.noreply.github.com>
2020-02-27 18:28:27 +0200

    v1

---
+++ script.js
@@ -0,0 +1,48 @@
+$("document").ready(function(){
+	$(".box6-simple-btn").click(function(){
+		let x = this.id;
+		if (x == "box6-simple-btn1") {
+			document.getElementById("box6-simple-main1").style.display = "block";
+			document.getElementById("box6-simple-main2").style.display = "none";
+			document.getElementById("box6-simple-main3").style.display = "none";
+
+			document.getElementById("box6-more1").style.display = "block";
+			document.getElementById("box6-more2").style.display = "none";
+			document.getElementById("box6-more3").style.display = "none";
+
+            document.getElementById("box6-more1").style.display = "block";
+			document.getElementById("box6-more2").style.display = "none";
+			document.getElementById("box6-more3").style.display = "none";
+			
+			document.getElementById("box6-simple-btn1").style.background = "#ddd"; //светлый
+			document.getElementById("box6-simple-btn2").style.background = "#bbb"; //темный
+			document.getElementById("box6-simple-btn3").style.background = "#bbb"; //темный
+		}
+		else if(x == "box6-simple-btn2"){
+			document.getElementById("box6-simple-main1").style.display = "none";
+			document.getElementById("box6-simple-main2").style.display = "block";
+			document.getElementById("box6-simple-main3").style.display = "none";
+
+			document.getElementById("box6-more1").style.display = "none";
+			document.getElementById("box6-more2").style.display = "block";
+			document.getElementById("box6-more3").style.display = "none";
+
+			document.getElementById("box6-simple-btn1").style.background = "#bbb"; //темный
+			document.getElementById("box6-simple-btn2").style.background = "#ddd"; //светлый
+			document.getElementById("box6-simple-btn3").style.background = "#bbb"; //темный
+		}
+		else if(x == "box6-simple-btn3"){
+			document.getElementById("box6-simple-main1").style.display = "none";
+			document.getElementById("box6-simple-main2").style.display = "none";
+			document.getElementById("box6-simple-main3").style.display = "block";
+
+			document.getElementById("box6-more1").style.display = "none";
+			document.getElementById("box6-more2").style.display = "none";
+			document.getElementById("box6-more3").style.display = "block";
+
+			document.getElementById("box6-simple-btn1").style.background = "#bbb"; //темный
+			document.getElementById("box6-simple-btn2").style.background = "#bbb"; //темный
+			document.getElementById("box6-simple-btn3").style.background = "#ddd"; //светлый
+		}
+	})
+})
